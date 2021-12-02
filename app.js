9d4fac75e643eba21dc03aa24a4e298b5728b819
Catrina Alex <ovidiualex.catrina@gmail.com>
2021-12-02 12:02:05 +0200

    other commit

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-let chckbox=document.getElementById('generalcf');
+let chckbox=document.querySelector(".generalcf");
 let container=document.querySelector(".container");
 
 //toate tagurile din div
-let textfrombox = document.getElementById('pp');
-let prgf=document.getElementById('paragraf');
-let lbl=document.getElementById('cflabel');
-let confirmedbox=document.getElementById('cf');
-let btnedit=document.getElementById('btnedit');
-let btnremove=document.getElementById('btnremove');
+let textfrombox = document.querySelector(".pp");
+let prgf=document.querySelector(".paragraf");
+let lbl=document.querySelector(".cflabel");
+let confirmedbox=document.querySelector(".cf");
+let btnedit=document.querySelector(".btnedit");
+let btnremove=document.querySelector(".btnremove");
 let inputsbm=document.querySelector(".inputsb");
 
 //addaugare persoana -> butonul submit
-let btnsubmit=document.querySelector('btnsbmt')
+let btnsubmit=document.querySelector(".btnsbmt");
 
 
 let flag=1;
@@ -20,7 +20,7 @@ chckbox.addEventListener("change", ()=>{
 
     
     
-        if(flag==0 && confirmedbox.checked==false){
+        if(flag==0 && confirmedbox.checked==true){
             textfrombox.style.display = "block";
             /*prgf[i].style.display = "block";
             lbl[i].style.display = "block";
@@ -47,20 +47,47 @@ chckbox.addEventListener("change", ()=>{
 
 btnsubmit.addEventListener("click", ()=>{
 
-    let elemdiv=document.createElement("div");
-    
-    elemparagraf="Confirmed";
-    
+    let elemdiv=document.createElement("p");
 
     if(inputsbm.value!=="")
     {
-    elemdiv.textContent=inputsbm.value; 
-    elemdiv.append(elemdiv);
+    
+    elemdiv.textContent=inputsbm.value;
+
+    attachbuttons(elemdiv);
+
+    textfrombox.append(elemdiv);
+    
    
-           
-    inputsbm.value="";
     }
+    newitem.value="";
 
 
 })
+
+function attachbuttons(bts){
+
+    bts.classList.add("cflabel");
+    let lbl1=document.createElement('label');
+    lbl1.textContent="Confirmed";
+    bts.appendChild(lbl1); 
+
+    bts.classList.add("cf");
+    let conf1=document.createElement('input');
+    conf1.type="checkbox";
+    bts.appendChild(conf1);
+
+
+    bts.classList.add("btnedit");
+    let btn1=document.createElement('button');
+    btn1.textContent="edit";
+    bts.appendChild(btn1);
+
+    bts.classList.add("btnremove");
+    let btn2=document.createElement('button');
+    btn2.textContent="remove";
+    bts.appendChild(btn2);
+}
+    
+
 
